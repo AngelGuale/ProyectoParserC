1017a411efe573549fca239ca4a1e0bd81433789
CrobbyStyle <CrobbyStyle@users.noreply.github.com>
2015-07-06 13:32:33 -0500

    YA ESTA TODO, menos los errores, menos la grabacion en archivos a parte, menos el write-up...

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* 
  * File:   main.c
- * Author: xngel
+ * Author: xngel, roberto
  *
  * Created on 25 de junio de 2015, 05:23 PM
  */
@@ -13,70 +13,128 @@
 
 
 void menuPrincipal();
-int contarPalabras();
+int contarPalabras(char* textoArchivo, int numMayorQue);
+void cargarArchivo(char* nombreArchivo, char* textoArchivo);
+void voltearTexto(char* textoArchivo, char* textoModificado);
+void sinEspacios(char* textoArchivo, char* textoModificado);
 /*
- * prueba1.txt
- * 
- */
-int main(int argc, char** argv) { 
-    menuPrincipal();
-    system("PAUSE");      
+* prueba1.txt
+*
+*/
+int main(int argc, char** argv) {
+	menuPrincipal();
+	system("PAUSE");
 }
 
 void menuPrincipal(){
-    printf("\t\t\tBIENVENIDOS AL CONTADOR DE PALABRAS O-PE\n\n\n" ); 
-    printf("Menu Principal\n\n");
-    printf("[1]Contar palabras de un archivo\n");
-    printf("[2]Contar palabras por caracteres\n");
-    int op,i;
-    printf("Escoja una opcion:  ");
-    scanf("%d",&op);
-    switch(op){
-        case 1:           
-            i = contarPalabras();
-            printf("El total de palabras es: %d\n",i);
-            break;
-        case 2:
-            printf("jajaja");
-            break;
-        default:
-            printf("opcion invalida\n");
-            break;
-    } 
+	char* nombreArchivo = malloc(sizeof(char*));
+	char* textoArchivo[500];
+	char* textoModificado[500];
+	textoArchivo[0] = "&";
+	printf("\t\t\tBIENVENIDOS AL CONTADOR DE PALABRAS O-PE\n\n\n");
+	printf("Menu Principal\n\n");
+	while (textoArchivo[0] == "&"){
+		printf("Ingrese el nombre del archivo:");
+		scanf("%s", nombreArchivo);
+		cargarArchivo(nombreArchivo, textoArchivo);
+	}
+	int op = 0, i, numMayorQue;
+	while (op >= 0){
+		printf("[1]Contar palabras de un archivo\n");
+		printf("[2]Contar palabras por caracteres\n");
+		printf("[3]Voltear el texto\n");
+		printf("[4]Quitar espacios\n");
+		printf("[5]Salir\n");
+		printf("Escoja una opcion:  ");
+		scanf("%d", &op);
+		
+		switch (op){
+		case 1:
+			i = contarPalabras(textoArchivo, 0);
+			printf("El total de palabras es: %d\n", i);
+			break;
+		case 2:
+			printf("Ingrese el numero minimo de caracteres que no desea contar: ");
+			scanf("%d", &numMayorQue);
+			i = contarPalabras(textoArchivo, numMayorQue);
+			printf("El total de palabras mayores a %d caracteres es: %d\n", numMayorQue, i);
+			break;
+		case 3:
+			printf("Escribiendo desde el final hasta el inicio.\n");
+			voltearTexto(textoArchivo, textoModificado);
+			printf("El texto volteado es:\n %s\n", textoModificado);
+			break;
+		case 4:
+			printf("Escribiendo desde el final hasta el inicio.\n");
+			sinEspacios(textoArchivo, textoModificado);
+			printf("El texto volteado es:\n %s\n", textoModificado);
+			break;
+		case 5:
+			printf("Saliendo...");
+			op = -1;
+			break;
+		default:
+			printf("opcion invalida\n");
+			if (op<1 || op>5){
+				op = 0;
+				printf("Escoja una opcion:  ");
+				scanf("%d", &op);
+			}
+			break;
+		}
+	}
 }
 
-int contarPalabras(){ 
-    FILE * fichero;
-    char* nombreArchivo=malloc(sizeof(char*));   
-    printf("Ingrese el nombre del archivo:");
-    scanf("%s",nombreArchivo);
-    fichero= fopen (nombreArchivo,"r");
-    printf("%s\n", nombreArchivo);
-    char c;    
-    char buffer[500];  
-    fseek(fichero, 0, SEEK_END); // seek to end of file
-    int size = ftell(fichero); // get current file pointer
-    fseek(fichero, 0, SEEK_SET); // seek back to beginning of file
-    //printf("%d",size);
-    int totalPalabras=0;
-    int i=0;
-    if (fichero==NULL){
-        return 0;
-    }else{        
-        while(!feof(fichero)){
-            fscanf(fichero,"%[^'\0']",buffer);       
-            //printf("%s",buffer );
-        }
-            char* ptr=buffer;          
-            while(ptr[i] && i<500){
-                c= ptr[i];
-                i++;
-                if(isspace(c)){
-                    totalPalabras++;
-                }else{
-                    printf("");
-                }
-           }
-    }
-    return totalPalabras;
+int contarPalabras(char* textoArchivo, int numMayorQue){
+	int totalPalabras = 0;
+	int i = 0;
+	int numLetra = 0;
+	while(textoArchivo[i]!='\0'){
+		if (textoArchivo[i] == ' ' && numLetra > 0){
+			if (numLetra > numMayorQue)
+				totalPalabras++;
+			numLetra = 0;
+		}
+		else
+			numLetra++;
+		i++;
+	}
+	return totalPalabras;
+}
+
+void cargarArchivo(char* nombreArchivo, char* textoArchivo){
+	FILE* fichero;
+	fichero = fopen(nombreArchivo, "r");
+	printf("%s\n", nombreArchivo);
+	char c;
+	if (fichero == NULL){
+		printf("Archivo no encontrado. Intente de nuevo.\n");
+		textoArchivo[0] = "&";
+	}
+	else{
+		while (!feof(fichero)){
+			fscanf(fichero, "%[^'\0']", textoArchivo);
+		}
+	}
+}
+
+void voltearTexto(char* textoArchivo, char* textoModificado){
+	int i = 0;
+	for (i = 0; i< strlen(textoArchivo); i++){
+		textoModificado[strlen(textoArchivo) - i - 1] = textoArchivo[i];
+	}
+	textoModificado[strlen(textoArchivo)] = '\0';
+
+}
+
+void sinEspacios(char* textoArchivo, char* textoModificado){
+	int i = 0, j = 0;
+	while (textoArchivo[i] != '\0'){
+		if (textoArchivo[i] != ' '){
+			textoModificado[j] = textoArchivo[i];
+			j++;
+		}
+		i++;
+	}
+	textoModificado[j] = '\0';
 }
